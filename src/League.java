2c597c313392eb37fa553f2c95d790f78b70d9fa
Vahid Nasri <vahidffs@gmail.com>
2020-01-25 15:14:25 -0500

    Make Base classes for the league

--- src/League.java
+++ src/League.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
 
-public abstract class League {
-
+public class League <T> {
+	String name;
+	ArrayList<T> teams = new ArrayList<>();
+	public League(String name) {
+		this.name = name;
+	}
+	public boolean addTeam(T team) {
+		if(teams.contains(team)) {
+			return false;
+		}else {
+			teams.add(team);
+			return true;
+		}
+		
+	}
+	public void printLeague() {
+		
+	}
 }
